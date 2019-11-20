d2934383bb9956446a1bcd72f70d24aa419e08e6
AlfredYang1986 <yangyuanpig@gmail.com>
2019-11-20 20:26:32 +0800

    await 的理解

--- src/index.ts
+++ src/index.ts
@@ -105,88 +105,98 @@ const gpFiles = R.groupWith((left: Entry, right: Entry) => {
 // 5. 对每一个File，创建一个Asset
 // tslint:disable-next-line:no-var-requires
 const sleep = require('sleep')
-Promise.all( R.map( async (arrs: Entry[]) => {
-    const et = arrs[0]
-    const fileName = et.filePath.substr(et.filePath.lastIndexOf("/") + 1)
-    const filePath = fileName // et.filePath
-    const name = new Date().getTime()
-    const jobId = uuidv4()
-    const uploadLink = jobId + "/" + name
 
-    PhLogger.info(et.filePath)
+async function upFiles(slice: Entry[][]) {
+    await Promise.all( slice.map( async (arrs: Entry[]) => {
+        const et = arrs[0]
+        const fileName = et.filePath.substr(et.filePath.lastIndexOf("/") + 1)
+        const filePath = fileName // et.filePath
+        const name = new Date().getTime()
+        const jobId = uuidv4()
+        const uploadLink = jobId + "/" + name
 
-    /**
-     * 6. 防止重复上传
-     */
-    const isExist = await am.findOne({
-        name: fileName
-    }).exec()
-
-    if (isExist !== null) {
-        return isExist
-    } else {
-        /**
-         * 5.1 assets 的问题
-         */
-        const asset = new Asset()
-        asset.traceId = jobId
-        asset.name = fileName
-        asset.description = et.filePath
-        asset.owner = "auto robot"
-        asset.accessibility = "w"
-        asset.version = 0
-        asset.isNewVersion = true
-        asset.dataType = "file"
-
-        asset.providers = [et.companyName]
-        asset.markets = []
-        asset.molecules = []
-        asset.dataCover = et.dataCover.toString().trim().split("-")
-        asset.geoCover = []
-        asset.labels = [et.label]
+        PhLogger.info(et.filePath)
 
         /**
-         * 5.3 优先上传文件
+         * 6. 防止重复上传
          */
-        let point = null
-        // const r2 = await ossClient.multipartUpload( uploadLink, "tmp/" + filePath, {
-        const r2 = await ossClient.multipartUpload( uploadLink, et.filePath, {
-            parallel: 5, // 并行上传的分片个数
-            partSize: 100 * 1024 * 1024, // 分片大小不能小于1024*100
-            checkpoint: point,
-            async progress ( p, checkpoint, res ) {
-                // debugger
-                point = checkpoint
-                PhLogger.info( p )
+        const isExist = await am.findOne({
+            name: fileName
+        }).exec()
+
+        if (isExist !== null) {
+            return isExist
+        } else {
+            /**
+             * 5.1 assets 的问题
+             */
+            const asset = new Asset()
+            asset.traceId = jobId
+            asset.name = fileName
+            asset.description = et.filePath
+            asset.owner = "auto robot"
+            asset.accessibility = "w"
+            asset.version = 0
+            asset.isNewVersion = true
+            asset.dataType = "file"
+
+            asset.providers = [et.companyName]
+            asset.markets = []
+            asset.molecules = []
+            asset.dataCover = et.dataCover.toString().trim().split("-")
+            asset.geoCover = []
+            asset.labels = [et.label]
+
+            /**
+             * 5.3 优先上传文件
+             */
+            let point = null
+            // const r2 = await ossClient.multipartUpload( uploadLink, "tmp/" + filePath, {
+            const r2 = await ossClient.multipartUpload( uploadLink, et.filePath, {
+                parallel: 5, // 并行上传的分片个数
+                partSize: 100 * 1024 * 1024, // 分片大小不能小于1024*100
+                checkpoint: point,
+                async progress ( p, checkpoint, res ) {
+                    // debugger
+                    point = checkpoint
+                    PhLogger.info( "upload progress " + p )
+                }
+            } )
+
+            if (r2.res.status !== 200) {
+                PhLogger.error("upload to oss error: " + r2.res.status + " with file: " + et.filePath )
+                process.exit(-1)
             }
-        } )
 
-        if (r2.res.status !== 200) {
-            PhLogger.error("upload to oss error: " + r2.res.status + " with file: " + et.filePath )
-            process.exit(-1)
+            /**
+             * 5.2 创建Files 的问题
+             */
+            const file = new File()
+            file.fileName = fileName // et.filePath
+            file.extension = et.filePath.substr(et.filePath.lastIndexOf(".") + 1)
+            file.uploaded = new Date().getTime()
+            file.size = -1
+            file.url = uploadLink
+
+            asset.file = await fm.create(file)
+
+            sleep.sleep(1)
+            return await am.create(asset)
         }
+    } ) )
 
-        /**
-         * 5.2 创建Files 的问题
-         */
-        const file = new File()
-        file.fileName = fileName // et.filePath
-        file.extension = et.filePath.substr(et.filePath.lastIndexOf(".") + 1)
-        file.uploaded = new Date().getTime()
-        file.size = -1
-        file.url = uploadLink
-
-        asset.file = await fm.create(file)
-
-        sleep.sleep(1)
-        return await am.create(asset)
-    }
-}, gpFiles ) ).then(assets => {
-    PhLogger.info(assets)
-    PhLogger.info(assets.length)
     // last, disconnect the database
-    mongoose.disconnect()
-} ).catch(err => {
-    PhLogger.error(err)
-    mongoose.disconnect()
-} )
+    // mongoose.disconnect()
+}
+
+async function multiRound() {
+    const step = 2
+    let idx = 0
+    while (idx + step < gpFiles.length) {
+        await upFiles(R.slice(idx, idx + step, gpFiles))
+        idx += step
+    }
+
+}
+
+multiRound()
